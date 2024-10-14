ae6949d3ec788540c2ba823b69fba07f9ccdd650
shopsyncsolutions <shopsyncsolutions@gmail.com>
2024-10-14 14:58:22 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,308 @@
+
+<!doctype html>
+<html lang="pt">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Vitality BluePrint</title>
+
+    <!-- Incluindo arquivos CSS -->
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.6.0/css/all.css">
+</head>
+
+<body>
+
+    <div class="container-fluid">
+        <div class="row">
+
+            <div class="conteudo_antes ">
+
+                <!-- HEADLINE -->
+                <div class="conteudo">
+                    <h3>Silent <strong class="text-white">Killer</strong> of Your Bedroom <br>Confidence—Here’s How You
+                        Can <strong class="text-white">Stop It for Good!</strong></h3>
+
+                    <h4 class="mb-5">
+                        <?php
+                            date_default_timezone_set('UTC');
+                            $datetime = new DateTime();
+                            echo $datetime->format('m/d/y');
+                        ?>
+                    </h4>
+
+                </div>
+
+                <!-- VIDEO -->
+              <div id="vid_6705b4047f7358000b44b8f9" style="position:relative;width:100%;padding: 177.63157894736844% 0 0;"> <img id="thumb_6705b4047f7358000b44b8f9" src="https://images.converteai.net/d550de32-02eb-4588-962a-07d9e914f9cb/players/6705b4047f7358000b44b8f9/thumbnail.jpg" style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;display:block;"> <div id="backdrop_6705b4047f7358000b44b8f9" style="position:absolute;top:0;width:100%;height:100%;-webkit-backdrop-filter:blur(5px);backdrop-filter:blur(5px);"></div> </div> <script type="text/javascript" id="scr_6705b4047f7358000b44b8f9"> var s=document.createElement("script"); s.src="https://scripts.converteai.net/d550de32-02eb-4588-962a-07d9e914f9cb/players/6705b4047f7358000b44b8f9/player.js", s.async=!0,document.head.appendChild(s); </script> <style> .elementor-element:has(#smartplayer) { width: 100%; } </style>
+              
+                <!-- CONTADOR -->
+                <div class="container">
+                    <div class="people-watching">
+                        People watching now: <span id="counter" class="counter">0</span>
+                    </div>
+                </div>
+
+                <div class="bg-logos">
+                    <!-- LOGOS -->
+                    <h3>Scientific References</h3>
+                    <div class="container text-center logos">
+                        <div class="row">
+                            <div class="col-6 col-md-auto">
+                                <img src="1.png" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-6 col-md-auto">
+                                <img src="2.png" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-6 col-md-auto">
+                                <img src="3.png" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-6 col-md-auto">
+                                <img src="4.png" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-6 col-md-auto">
+                                <img src="5.png" alt="" class="img-fluid">
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+
+
+            </div>
+
+            <div id="conteudo_depois">
+
+                <!-- Tabela preço -->
+                <div class="row table-price">
+
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div class="pricing-card">
+                            <div class="card border-light">
+                                <header class="card-header border-bottom bg-white text-center">
+
+                                    <div class="card-header bg-dark m-1">
+                                        <h4 class="my-0 font-weight-bold">Try One</h4>
+                                    </div>
+
+                                    <img src="1bottle.png" alt="1 Bottle">
+                                    <h1 class="p-4">$89<small> Per bottle</small>
+                                    </h1>
+                                </header>
+                                <div class="card-body" style="text-align: center">
+                                    <div class="d-flex">
+                                        <div class="icon-md icon-secondary lh-180 mr-3"><span
+                                                class="fas fa-check-circle"></span></div>
+                                        <p>30-Day Supply</p>
+                                    </div>
+                                    <div class="d-flex">
+                                        <div class="icon-md icon-secondary lh-180 mr-3 text-danger"><span
+                                                class="fa-solid fa-circle-xmark"></span></div>
+                                        <p class="text-danger">Free US Shipping</p>
+                                    </div>
+                                    <div class="d-flex">
+                                        <div class="icon-md icon-secondary lh-180 mr-3 text-danger"><span
+                                                class="fa-solid fa-circle-xmark"></span></div>
+                                        <p class="text-danger">2 Free E-Books</p>
+                                    </div>
+
+                                    <a href="https://www.checkout-ds24.com/product/559322?aff=VT-TTBS&cam=CAMPAIGNKEY&utm_source=120212914873300062"
+                                        target="_blank" rel="noopener noreferrer">
+                                        <button type="button" class="btn-block mt-4 btn-buy">
+                                            <span class="fas fa-cart-plus mr-3"></span>Add to Cart
+                                        </button>
+                                    </a>
+
+                                    <div class="row p-3 justify-content-center text-center">
+                                        <img src="cards-2.webp" height="60px" width="auto">
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div class="pricing-card destaque">
+                            <div class="card">
+
+                                <div class="card-header bg-danger m-1">
+                                    <h4 class="my-0 font-weight-bold">Best Value</h4>
+                                </div>
+
+                                <header class="text-center">
+                                    <img src="6bottles.png" alt="6 Bottle" class="destaque_bottles">
+                                    <h1 class="p-4">$49 <small> Per bottle</small>
+                                    </h1>
+                                </header>
+                                <hr>
+                                <div class="card-body" style="text-align: center">
+                                    <div class="d-flex">
+                                        <div class="icon-md icon-secondary lh-180 mr-3"><span
+                                                class="fas fa-check-circle"></span></div>
+                                        <p>180-Day Supply</p>
+                                    </div>
+                                    <div class="d-flex">
+                                        <div class="icon-md icon-secondary lh-180 mr-3"><span
+                                                class="fas fa-check-circle"></span></div>
+                                        <p>Free US Shipping</p>
+                                    </div>
+                                    <div class="d-flex">
+                                        <div class="icon-md icon-secondary lh-180 mr-3"><span
+                                                class="fas fa-check-circle"></span></div>
+                                        <p>2 Free E-Books</p>
+                                    </div>
+
+                                    <a href="https://www.checkout-ds24.com/product/559324?aff=VT-TTBS&cam=CAMPAIGNKEY&utm_source=120212914873300062"
+                                        target="_blank" rel="noopener noreferrer">
+                                        <button type="button" class="btn-buy btn-block mt-4">
+                                            <span class="fas fa-cart-plus mr-3"></span>Add to Cart
+                                        </button>
+                                    </a>
+
+                                    <div class="row p-3 justify-content-center text-center">
+                                        <img src="cards-2.webp" height="60px" width="auto">
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div class="pricing-card">
+                            <div class="card border-light">
+                                <header class="card-header border-bottom bg-white text-center">
+
+                                    <div class="card-header bg-danger m-1">
+                                        <h4 class="my-0 font-weight-bold">Most Popular</h4>
+                                    </div>
+
+                                    <img src="3bottles.png" alt="3 Bottle">
+                                    <h1 class="p-4">$59<small> Per bottle</small>
+                                    </h1>
+                                </header>
+                                <div class="card-body" style="text-align: center">
+                                    <div class="d-flex">
+                                        <div class="icon-md icon-secondary lh-180 mr-3"><span
+                                                class="fas fa-check-circle"></span></div>
+                                        <p>90-Day Supply</p>
+                                    </div>
+                                    <div class="d-flex">
+                                        <div class="icon-md icon-secondary lh-180 mr-3"><span
+                                                class="fas fa-check-circle"></span></div>
+                                        <p>Free US Shipping</p>
+                                    </div>
+                                    <div class="d-flex">
+                                        <div class="icon-md icon-secondary lh-180 mr-3"><span
+                                                class="fas fa-check-circle"></span></div>
+                                        <p>2 Free E-Books</p>
+                                    </div>
+
+                                    <a href="https://www.checkout-ds24.com/product/559323?aff=VT-TTBS&cam=CAMPAIGNKEY&utm_source=120212914873300062"
+                                        target="_blank" rel="noopener noreferrer">
+                                        <button type="button" class="btn-buy btn-block">
+                                            <span class="fas fa-cart-plus"></span>Add to Cart
+                                        </button>
+                                    </a>
+
+                                    <div class="row p-3 justify-content-center text-center">
+                                        <img src="cards-2.webp" height="60px" width="auto">
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="container mt-3 mb-3 ">
+                    <div class="row mb-3 justify-content-center text-center">
+                        <div class="col-auto">
+                            <img src="logo-garantia/usa.png" alt="">
+                        </div>
+                        <div class="col-auto">
+                            <img src="logo-garantia/fda.png" alt="">
+                        </div>
+                        <div class="col-auto">
+                            <img src="logo-garantia/cgmp.png" alt="">
+                        </div>
+                    </div>
+                    <div class="row justify-content-center">
+                        <div class="col-auto">
+                            <img src="logo-garantia/gif.png" alt="">
+                        </div>
+                        <div class="col-auto">
+                            <img src="logo-garantia/bpa.png" alt="">
+                        </div>
+                        <div class="col-auto">
+                            <img src="logo-garantia/gmo.png" alt="">
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <footer class="footer">
+                <div class="container">
+                    <p class="footer-text">
+                        All statements and results presented on this website are for informational purposes only.
+                        They
+                        are not specific medical advice for any individual. Neither the website, nor product should
+                        substitute medical advice given by a certified health professional. If you have a health
+                        problem, or you have sensible allergies, are pregnant or diagnosed with chronic conditions,
+                        it
+                        is strongly recommended that you consult your doctor immediately and before taking any pills
+                        or
+                        supplements. Individual results may vary depending on the case.
+                        <br><br>
+                        Always consult your doctor about any issues related to your health and your treatments and
+                        medications When addressing financial issues, any of our products, websites, videos,
+                        conferences, programs or other content, we do our best to ensure that they faithfully
+                        represent
+                        our courses and your ability to improve your life. However. IMAGES MERELY ILLUSTRATIVE.
+                    <p class="text-center">© Truesterone. All Rights Reserved.</p>
+                    </p> 
+                </div>
+            </footer>
+
+        </div>
+
+        <script>
+        // Função para incrementar o contador
+        let count = 80;
+
+        function updateCounter() {
+            if (count < 36587) {
+                count += 4;
+                document.getElementById('counter').innerText = count;
+            } else {
+                clearInterval(counterInterval); // Para o contador ao chegar em 100
+            }
+        }
+        // Incrementar o contador a cada 2 segundos
+        setInterval(updateCounter, 1000);
+
+
+          //Aguarda 30 segundos antes de exibir o conteúdo
+          setTimeout(function() {
+              document.getElementById('conteudo_depois').style.display = 'block';
+          }, 861000);
+        </script>
+
+
+
+
+        <!-- Bootstrap JS -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+        </script>
+</body>
+
+</html>
